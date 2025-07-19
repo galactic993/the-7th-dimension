c23e75ccdee04de0708ca5de3a0db62ff9bf28f7
galactic993 <contact@mezame.ai>
2025-07-19 23:20:37 +0900

    auth.config.tsのドメイン設定を環境変数に対応させ、デフォルト値を保持するように変更

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://settled-dassie-49.clerk.accounts.dev",
+      domain: process.env.CLERK_FRONTEND_API_URL || "https://settled-dassie-49.clerk.accounts.dev",
       applicationID: "convex",
     },
   ]
